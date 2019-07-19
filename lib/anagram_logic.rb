881a7272b8f2c93b4d4add2ce38ca258ab157606
Marina Poltorak <emylisfamily@gmail.com>
2019-07-19 10:48:28 -0700

    writing tests and logic

--- lib/anagram_logic.rb
+++ lib/anagram_logic.rb
@@ -1,3 +1,38 @@
-def anagram(words)
+class AnagramSolver
+  attr_reader(:text, :letter_frequencies, :is_valid, :letters_in_text)
 
+  def initialize(text)
+    @text = text.downcase
+    @letter_frequencies = Hash.new { |hash, key| hash[key] = 0 }
+    ('a'..'z').each { |letter| @letter_frequencies[letter] }
+    @letters_in_text = []
+    @text.split('').each do |letter|
+      @letter_frequencies[letter] += 1
+      if !@letters_in_text.include?(letter)
+        @letters_in_text.push(letter)
+      end
+    end
+    letters_in_text.sort!
+    puts "@letters_in_text: #{@letters_in_text}"
+    puts "vowels in text: #{@letters_in_text & ['a', 'e', 'i', 'o', 'u', 'y']}"
+    @is_valid = @letters_in_text & ['a', 'e', 'i', 'o', 'u', 'y'] != []
+  end
+
+  def is_anagram(otherPhrase)
+    if (!@is_valid) || (!otherPhrase.is_valid)
+      return "Please enter real words"
+    end
+
+    if @letters_in_text & otherPhrase.letters_in_text == []
+      return "These are antigrams."
+    end
+
+    @letter_frequencies.each do |letter, frequency|
+      if frequency != otherPhrase.letter_frequencies[letter]
+        return "Nope, not anagrams."
+      end
+    end
+
+    "You got anagrams."
+  end
 end
--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -1,6 +1,35 @@
-require('rspec')
-require('anagram')
+require 'rspec'
+require 'pry'
+require 'anagram_logic'
 
-describe('#anagram') do
-  # eventually put specs here
+describe('AnagramSolver#anagram') do
+  it('Returns "You got anagrams!" when object is "listen" and argument is "silent"') do
+    phrase1 = Phrase.new("listen")
+    phrase2 = Phrase.new("silent")
+    expect(phrase1.anagram(phrase2)).to(eq("You got anagrams!"))
+  end
+
+  it('Returns "Nope, not anagrams!" when object is "cat" and argument is "not"') do
+    phrase1 = Phrase.new("cat")
+    phrase2 = Phrase.new("not")
+    expect(phrase1.anagram(phrase2)).to(eq("Nope, not anagrams!"))
+  end
+
+  it('Returns "You got anagrams!." when object is "Listen" and argument is "Silent"') do
+    phrase1 = Phrase.new("Listen")
+    phrase2 = Phrase.new("Silent")
+    expect(phrase1.anagram(phrase2)).to(eq("You got anagrams!"))
+  end
+
+  it('Returns "Please enter real words" when object is "brbrbr" and argument is "strp"') do
+    phrase1 = Phrase.new("brbrbr")
+    phrase2 = Phrase.new("strp")
+    expect(phrase1.anagram(phrase2)).to(eq("Please enter real words"))
+  end
+
+  it('Returns "These are antigrams" when object is "Hello, sweetie" and argument is "bai bai"') do
+    phrase1 = Phrase.new("Hello, sweetie")
+    phrase2 = Phrase.new("bai bai")
+    expect(phrase1.anagram(phrase2)).to(eq("These are antigrams"))
+  end
 end
